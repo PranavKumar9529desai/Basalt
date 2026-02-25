2b08088b550d1bd4454f9497daff12b2080202b0
Pranavkumar <dpranav7745@gmail.com>
2026-02-25 22:49:18 +0530

    feat : added the commandpallate to the __root.tsx

--- apps/tauri/src/routes/__root.tsx
+++ apps/tauri/src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { ThemeSelect } from "../app-shell/ThemeSelect";
 import { AppCommands } from "../commands/app-commands";
+import { EditorCommandPalette } from "../features/editor/components/EditorCommandPalette";
 
 export const Route = createRootRoute({
   component: () => (
     <div className="p-4 flex flex-col min-h-screen bg-[var(--sat-surface-1)] text-[var(--sat-text-primary)]">
       <AppCommands />
+      <EditorCommandPalette />
       <div className="flex gap-4 p-2 border-b border-[var(--sat-layout-border)] mb-4 items-center">
         <Link
           to="/"
