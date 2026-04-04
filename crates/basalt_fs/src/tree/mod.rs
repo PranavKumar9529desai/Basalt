6fb37cb5d6b0cfbd7e92eb03297a9ff07b4038f8
PranavKumar <dpranav7744@gmail.com>
2026-04-04 16:57:09 +0530

    chore(crates): remove old underscore-named crates, finalize restructure

--- crates/basalt_fs/src/tree/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod build;
-pub mod types;
-
-pub use build::build_flat_tree;
-pub use types::{FlatTreeNode, NodeKind};
--- crates/basalt_search/src/tantivy/mod.rs
+++
@@ -1,6 +0,0 @@
-mod schema;
-pub mod index;
-pub mod snippets;
-
-pub use index::TantivyIndex;
-pub use snippets::extract_snippets;
